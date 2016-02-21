158c879a728813cd006f62a472f10d25e5f88728
ExplodingCamera <mail@explodingcamera.com>
2016-02-21 22:07:22 +0100

    AirBnb style

--- src/installPlugin.js
+++ src/installPlugin.js
@@ -2,39 +2,41 @@ var npmi = require('npmi');
 var fs = require('fs');
 
 module.exports = function (module, type) {
-  if(type == 'plugin')
-    if(module.indexOf("padplus-plugin") == -1 && module.indexOf("/") == -1)
-      module = "padplus-plugin-".concat(module);
+  if (type == 'plugin')
+    if (module.indexOf('padplus-plugin') == -1 && module.indexOf('/') == -1)
+      module = 'padplus-plugin-'.concat(module);
   var options = {
-      name: module,
-      path: process.cwd(),
-      forceInstall: false,
-      npmLoad: {
-          loglevel: 'info'
-      }
+    name: module,
+    path: process.cwd(),
+    forceInstall: false,
+    npmLoad: {
+      loglevel: 'info',
+    },
   };
   npmi(options, function (err, result) {
+    if (err) {
+      if (err.code === npmi.LOAD_ERR)    console.log('npm load error');
+      else if (err.code === npmi.INSTALL_ERR) console.log('npm install error');
+      return console.log(err.message);
+    }
+
+    module.replace('padplus-plugin-', '');
+    fs.readFile(process.cwd() + '/padplus.config.json', function read(err, data) {
       if (err) {
-          if      (err.code === npmi.LOAD_ERR)    console.log('npm load error');
-          else if (err.code === npmi.INSTALL_ERR) console.log('npm install error');
-          return console.log(err.message);
+        throw err;
       }
 
-      module.replace("padplus-plugin-", "");
-      fs.readFile(process.cwd()+"/padplus.config.json", function read(err, data) {
-          if (err) {
-              throw err;
-          }
-          if(type == 'plugin')
-            processConfig(data);
-      });
-      var processConfig = function (content) {
+      if (type == 'plugin')
+        processConfig(data);
+    });
+
+    var processConfig = function (content) {
         content = JSON.parse(content);
-        if(content.plugins.indexOf(module) == -1)
+        if (content.plugins.indexOf(module) == -1)
         content.plugins.push(module);
-        fs.writeFile(process.cwd()+"/padplus.config.json", JSON.stringify(content, null, 4), function(err) {
-          console.log(options.name+'@'+options.version+' installed successfully in '+ options.path);
+        fs.writeFile(process.cwd() + '/padplus.config.json', JSON.stringify(content, null, 4), function (err) {
+          console.log(options.name + '@' + options.version + ' installed successfully in ' + options.path);
         });
-      }
- });
-}
+      };
+  });
+};
--- src/setup.js
+++ src/setup.js
@@ -6,68 +6,69 @@ var download = require('download-git-repo');
 var installPlugin = require('./installPlugin.js');
 
 module.exports = function (options) {
-  if(typeof options.type == 'undefined') {
-    console.log(colors.green("No type provided, using default (full)"));
+  if (typeof options.type == 'undefined') {
+    console.log(colors.green('No type provided, using default (full)'));
     console.log();
-    options.type = "full";
+    options.type = 'full';
   }
+
   checkForMP();
-}
+};
 
 var dlMusiqPad = function () {
-  download('musiqpad/mqp-server', process.cwd()+"/musiqpad"/* <- text is just for testing, will be removed at beta release*/, function(err) {
+  download('musiqpad/mqp-server', process.cwd() + '/musiqpad'/* <- text is just for testing, will be removed at beta release*/, function (err) {
     if (err) {
-      console.log(colors.red('Error: ')+err);
-      process.exit()
-    }
-    else {
-      console.log("Succesfully downloaded MusiqPad, now installing PadPlus.");
+      console.log(colors.red('Error: ') + err);
+      process.exit();
+    } else {
+      console.log('Succesfully downloaded MusiqPad, now installing PadPlus.');
       installPadPlus();
     }
   });
-}
+};
 
 var dirIsEmpty = function () {
   var isEmpty = 0;
   extfs.isEmpty(process.cwd(), function (empty) {
     isEmpty = empty;
   });
+
   return isEmpty;
-}
+};
 
 var checkForMP = function () {
-  if(dirIsEmpty() == true) {
+  if (dirIsEmpty() == true) {
     console.log("MusiqPad is already installed or the dir isn't empty.");
-    console.log("We will now install PadPlus to " + process.cwd());
-    yesno.ask('Are you sure you want to continue? (y/n)', true, function(ok) {
-        if(ok) {
-            console.log("Ok, now installing PadPlus");
-            installPadPlus();
-        } else {
-            process.exit();
-        }
+    console.log('We will now install PadPlus to ' + process.cwd());
+    yesno.ask('Are you sure you want to continue? (y/n)', true, function (ok) {
+      if (ok) {
+        console.log('Ok, now installing PadPlus');
+        installPadPlus();
+      } else {
+        process.exit();
+      }
     });
-  }
-  else {
+  } else {
     dlMusiqPad();
   }
-}
+};
 
 var ConfigTemplate = {
-  version: "0.0.1",
+  version: '0.0.1',
   plugins: [
-    "explodingcamera/musiqplus"
-  ]
-}
+    'explodingcamera/musiqplus',
+  ],
+};
 
 var installPadPlus = function () {
-  fs.writeFile(process.cwd()+"/padplus.config.json", JSON.stringify(ConfigTemplate, null, 4), function(err) {
-      if(err) {
-          return console.log(err);
-      }
-      console.log("Created Config!");
-      ConfigTemplate.plugins.forEach(function (e) {
+  fs.writeFile(process.cwd() + '/padplus.config.json', JSON.stringify(ConfigTemplate, null, 4), function (err) {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log('Created Config!');
+    ConfigTemplate.plugins.forEach(function (e) {
         installPlugin(e, 'Plugin');
       });
   });
-}
+};
